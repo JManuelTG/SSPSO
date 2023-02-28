e86798f40c30e82e63f781a8dba1076a0ab79fc5
Jose Manuel Tarula <jose.tarula@alumnos.udg.mx>
2023-02-27 22:06:55 -0600

    Correccion de errores

--- Practica 2/main.py
+++ Practica 2/main.py
@@ -6,13 +6,14 @@ def siwtch_chars(path):
     out_lst = []
     new_word = ""
     letters = "abcdefghijklmnopqrstuvwxyz"
+
     for element in os.listdir(path):
         new_path =  os.path.join(path, element)
-        if os.path.isdir(element):
-            siwtch_chars(element)
+        if os.path.isdir(new_path):
+            siwtch_chars(new_path)
         else:
             if new_path.endswith(".txt"):
-                with open(new_path,"r") as file:
+                with open(new_path,"r+") as file:
                     for line in file:
                         for word in line.split(" "):
                             for char in word:
@@ -20,19 +21,17 @@ def siwtch_chars(path):
                                     char = random.choice(letters)
                                 elif char.isalpha():
                                     char = random.randint(1,9)
-                                else:
-                                    pass
                                 new_word = new_word + str(char)
                             out_lst.append(new_word)
                             new_word = ""
-                    with open(new_path,"a") as out:
-                        out.write("\n=================\n" + " ".join(x for x in out_lst))
+                    file.seek(0)
+                    file.write(" ".join(x for x in out_lst))
                     out_lst = []
-            else:
-                pass
+
        
 
 if __name__ == "__main__":
+
     directorio_original = './Archivos'
     directorio_copia = './Copias'
     shutil.copytree(directorio_original, directorio_copia)
