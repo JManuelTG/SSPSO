963194fffa6092b7ca27a7cf7a4fec98662a5a39
Jose Manuel Tarula <jose.tarula@alumnos.udg.mx>
2023-03-14 12:34:05 -0600

    Se agrega scrollbar a la tabla de procesos y se hace mas pequenio el frame de agregar procesos

--- Practica con GUI/app/app.py
+++ Practica con GUI/app/app.py
@@ -20,6 +20,7 @@ class App():
         #Panel iziquierdo
         sidebar = Frame(self.frame, bd=1, padx=40, bg="#FFFFFF",borderwidth=2)
         sidebar.pack(side=LEFT, fill=Y)
+
         #Etiqueta Opciones
         sidebar_label = Label(sidebar,text="Opciones", bg="#2F2F2F", foreground="#FFFFFF")
         sidebar_label.pack()
@@ -44,17 +45,17 @@ class App():
         frame_agregar = Frame(frame_table,padx=10,pady=10,height=100,width=200,bg="#D3D0CB")
         frame_agregar.pack(side=RIGHT, fill=Y, padx=10, pady=5)
 
-        id_proceso = Entry(frame_agregar, width=40)
+        id_proceso = Entry(frame_agregar, width=30)
         id_proceso.grid(row=0,column=1,pady=10)
         id_label = Label(frame_agregar, text="ID Proceso:", bg="#D3D0CB")
         id_label.grid(row=0,column=0,padx=20,pady=10)
 
-        tiempo = Entry(frame_agregar, width=40)
+        tiempo = Entry(frame_agregar, width=30)
         tiempo.grid(row=1,column=1,pady=10)
         tiempo_label = Label(frame_agregar, text="Tiempo:", bg="#D3D0CB")
         tiempo_label.grid(row=1,column=0,padx=20,pady=10)
 
-        prioridad = Entry(frame_agregar, width=40)
+        prioridad = Entry(frame_agregar, width=30)
         prioridad.grid(row=2,column=1,pady=10)
         prioridad_label = Label(frame_agregar, text="Prioridad:", bg="#D3D0CB")
         prioridad_label.grid(row=2,column=0,padx=20,pady=10)
@@ -62,10 +63,14 @@ class App():
         ttk.Button(frame_agregar, text="Agregar",bootstyle="success-outline", command=lambda: (self.planificadores.add_process(id_proceso.get(),int(tiempo.get()),int(prioridad.get())),
                                                            messagebox.showinfo("Aviso","Proceso agregado con exito"),
                                                            id_proceso.delete(0,"end"),tiempo.delete(0,"end"),
-                                                           prioridad.delete(0,"end"),self.actualizar_tabla())).grid(row=4,column=0,pady=10,padx=40)
+                                                           prioridad.delete(0,"end"),self.actualizar_tabla())).grid(row=4,column=0,pady=5,padx=30)
 
         # Crear objeto Treeview
-        self.table = ttk.Treeview(frame_table,yscrollcommand="none",show='headings',bootstyle="success")
+        self.table = ttk.Treeview(frame_table,yscrollcommand="none",show='headings',bootstyle="success",height=10)
+
+        scrollbar = ttk.Scrollbar(frame_table, orient="vertical", command=self.table.yview)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        self.table.configure(yscrollcommand=scrollbar.set)
 
         # Agregar columnas
         self.table['columns'] = ('ID', 'Tiempo', 'Prioridad')
@@ -77,7 +82,7 @@ class App():
         self.table.heading('ID', text='ID', anchor='center')
         self.table.heading('Tiempo', text='Tiempo', anchor='center')
         self.table.heading('Prioridad', text='Prioridad', anchor='center')
-
+        self.table.pack(fill=X,padx=10,pady=5)
         self.actualizar_tabla()
 
 
@@ -90,12 +95,13 @@ class App():
         abajo = Frame(content,bd=1,bg="#D3D0CB",borderwidth=2)
         abajo.pack(side=BOTTOM,fill=BOTH,expand=True)
 
+
     def actualizar_tabla(self):
         for row in self.table.get_children():
             self.table.delete(row)
         for item in self.planificadores.get_procesos():
             self.table.insert('','end', values=item)
-        self.table.pack(fill=X,padx=10,pady=5)
+        
 
 
 
